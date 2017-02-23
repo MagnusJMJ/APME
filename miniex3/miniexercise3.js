d97cdf831733ccd81630d7aabdec04201089d344
Magnus Johansen <magnus.jmj@gmail.com>
2017-02-23 11:23:43 +0100

    Update miniexercise3.js

--- miniex3/miniexercise3.js
+++ miniex3/miniexercise3.js
@@ -26,7 +26,7 @@ function draw() {
   planet(1000, 115, 5, '#4494d6');  // earth
   planet(750, 140, 3, '#bf5e0f');   // mars
   planet(5000, 165, 24, '#d8c5b6'); // jupiter
-  planet(4000, 210, 19, '#ccad70'); // saturn (doesn't have rings, you try coding that shit)
+  planet(4000, 210, 19, '#ccad70'); // saturn (doesn't have rings, sue me)
   planet(1200, 234, 9, '#3da4db');  // uranus
   planet(1000, 275, 6, '#2364d3');  // neptune
   planet(1000, 300, 2, '#bcbab1');  // pluto
