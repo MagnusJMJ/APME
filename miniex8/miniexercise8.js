7d9de9ecbc576e41d8064792b43a339e311a8b29
Magnus Johansen <magnus.jmj@gmail.com>
2017-03-29 22:35:45 +0200

    edits

--- miniex8/miniexercise8.js
+++ miniex8/miniexercise8.js
@@ -1,20 +1,36 @@
-var tree = [];
+var tree = [],
+    angle;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(150);
   stroke('white');
 
-  var trunkA = createVector(width/2, height    );
+  var slider = createSlider(0, PI, PI/4, 0.001);
+  slider.position(20, 20);
+  slider.style('width', '100px');
+  angle = slider.value();
+
+  var trunkA = createVector(width/2, height);
       trunkB = createVector(width/2, height/1.5);
 
-  tree.push(new Branch(trunkA, trunkB));
+  tree.push(new Branch(trunkA, trunkB, 10));
 }
 
-function mousePressed() {
-  for (i = tree.length-1; i >= 0; i--) {
-    tree[i].branch();
+function draw() {
+  background(150, 150);
+  for (i = 0; i < tree.length; i++) {
     tree[i].show();
+  }
+}
+
+function keyPressed() {
+  if (keyCode == 32) {
+    for (i = tree.length-1; i >= 0; i--) {
+      if (!tree[i].grown) {
+        tree[i].branch(angle);
+        tree[i].branch(-angle);
+      }
+    }
   }
 }
 
@@ -22,23 +38,18 @@ function Branch(from, to, thickness) {
   this.from = from;
   this.to = to;
   this.grown = false;
-  this.len = p5.Vector.sub(this.to, this.from);
-  this.thickness = thickness || 5;
+  this.thickness = thickness;
   this.show = function() {
-    push();
     strokeWeight(this.thickness)
     line(this.from.x, this.from.y, this.to.x, this.to.y);
-    pop();
   }
-  this.branch = function() {
-    if (!this.grown) {
+  this.branch = function(angle) {
       var dir = p5.Vector.sub(this.to, this.from);
-      dir.rotate(PI / 4);
+      dir.rotate(angle);
       dir.mult(0.67);
       var newEnd = p5.Vector.add(this.to, dir);
       tree.push(new Branch(this.to, newEnd, this.thickness*0.8));
       this.grown = true;
-    }
   }
 }
 
